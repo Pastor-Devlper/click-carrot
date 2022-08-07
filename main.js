661f77031b9c3e94afb9d92f9f0e2033e988c257
Taeseok Sim <ct1799@hanmail.net>
2022-08-07 22:00:27 +0900

    JS: algorism 01

---
+++ main.js
@@ -0,0 +1,21 @@
+// function: place bugs in random posiotion
+
+// funciton: place carrots in random posiotion
+
+// when push btn-play
+// 1. music start
+
+// 2. place bugs and carrots
+
+// 3. timer counts down
+
+// function: erase carrot
+//1. judge clicked carrot
+
+//2. correct:
+// erase the clicked carrot
+// count down num-remain on information
+
+//3. incorrent:
+// clicked bug; "you lost" in notice
+// reload button appear
