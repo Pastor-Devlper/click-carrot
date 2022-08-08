ab534397aabc751546a39ea20257de9bc7f9483a
Taeseok Sim <ct1799@hanmail.net>
2022-08-08 21:57:02 +0900

    JS: click Start -> placeRandom

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
-// function: place bugs in random posiotion
+// Start button clicked:
+const start = document.querySelector('.information__btn-play');
+const bugNodes = document.querySelectorAll('[src$="bug.png"]');
+const bugs = Array.from(bugNodes);
+const carrotsNodes = document.querySelectorAll('[src$="carrot.png"]');
+const carrots = Array.from(carrotsNodes);
+const targets = document.querySelector('.targets');
+const bgm = document.querySelector('.bgm');
 
-// funciton: place carrots in random posiotion
+start.addEventListener('click', () => {
+  // 01. place bugs and carrots in random posiotion
+  targets.style.display = 'block';
+  placeRandom(bugs);
+  placeRandom(carrots);
+  // 02. Music Start
+  bgm.play();
+  // 03. count down
+  timer(10);
+});
+
+function placeRandom(items) {
+  const innerWidth = window.innerWidth;
+  items.forEach((element) => {
+    element.style.position = 'absolute';
+    element.style.top = Math.round(Math.random() * innerWidth) + 'px';
+    element.style.left = Math.round(Math.random() * innerWidth) + 'px';
+  });
+}
+
+function timer(start) {
+  setInterval(() => {
+    for (let i = start, i > 0; i--) {
+      console.log(i);
+    }
+  }, 1000);
+}
 
 // when push btn-play
 // 1. music start
